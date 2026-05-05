78f027eba9a5e24224422f529eb195eb3907633e
Sam <74021826+x39OME@users.noreply.github.com>
2026-05-05 17:55:25 +0300

    Add files via upload

---
+++ personal-website-essam/src/components/contact/ContactMe.js
@@ -0,0 +1,39 @@
+import { Col, Row } from 'react-bootstrap';
+import navIcon6 from '../../assets/images/social/nav-icon6.svg'
+import navIcon7 from '../../assets/images/social/nav-icon7.svg'
+import navIcon8 from '../../assets/images/social/nav-icon8.svg'
+
+export const ContactMe = () => {
+
+  return (
+      <Col lg={12}>
+        <div id='connect' className='contact-me wow slideInUp'>
+          <Row>
+            <Col md={6} xl={4}>
+              <a href='mailto:essamabdullah@outlook.sa'>
+                <img src={navIcon7} alt='Email' />
+              </a>
+              <p>essamabdullah@outlook.sa</p>
+            </Col>
+            <Col md={6} xl={4}>
+              <a href='http://wa.me/+966505257849'>
+                <img src={navIcon6} alt='Whats App' />
+              </a>
+              <p>+966 505 257 849</p>
+            </Col>
+            <Col md={6} xl={4}>
+              <a href='https://www.linkedin.com/in/essam-abdullah-6an77aab/'>
+                <img src={navIcon8} alt='Linkedin' />
+              </a>
+              <p>Essam Abdullah</p>
+            </Col>
+          </Row>
+          <div className='shapes'>
+            <div className='circle'></div>
+            <div className='square'></div>
+            <div className='triangle'></div>
+          </div>
+        </div>
+      </Col>
+  )
+}
